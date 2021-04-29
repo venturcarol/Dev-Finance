200783598df906724ef1d657d67801876373f341
Carolina Ventura <carolventura1996@gmail.com>
2021-04-29 13:35:03 +0100

    creating form event

--- DevFinanças/scripts.js
+++ DevFinanças/scripts.js
@@ -4,34 +4,34 @@ const Modal = {
     // adicionar a class active ao modal
     document.querySelector('.modal-overlay').classList.add('active');
   },
+
   close(){
     // fechar o modal 
     // remover a class active do modal
     document.querySelector('.modal-overlay').classList.remove('active');
-  }
+  },
 }
 
 const transactions = [
   {
-    id: 1,
     description: 'Luz',
     amount: -50000,
     date: '23/01/2021',
   },
+
   {
-    id: 2,
     description: 'Website',
     amount: 50000,
     date: '23/01/2021',
   },
+
   {
-    id: 3,
     description: 'Internet',
     amount: -20000,
     date: '23/01/2021',
   },
+
   {
-    id: 4,
     description: 'App',
     amount: 200000,
     date: '23/01/2021',
@@ -39,30 +39,68 @@ const transactions = [
 ]
 
 const Transaction = {
+  all: transactions, 
+
+  add(transaction){
+    Transaction.all.push(transaction)
+    App.reload()
+  },
+
+  remove(index) {
+    Transaction.all.splice[index, 1]
+
+    App.reload()
+  },
+
   incomes(){
-    // somar as entradas
+    let income = 0;
+    // pegar todas as transações
+    // para cada transação, 
+    Transaction.all.forEach(transaction => {
+      //se ela for maior que zero
+      if(transaction.amount > 0) {
+        // somar a uma variavel e retornar a variavel
+        income += transaction.amount;
+      }
+    })
+
+    return income;
   },
+
   expenses(){
-    // somar as Saídas
+    let expense = 0; 
+      Transaction.all.forEach(transaction => {
+        if(transaction.amount < 0) {
+          expense += transaction.amount;
+        }
+      })
+      return expense;
   },
+
   total(){
-    //entradas - saídas
+    //entrada + saída
+    return Transaction.incomes() + Transaction.expenses();
   }
 }
 
 const DOM = {
   transactionsContainer: document.querySelector('#data-table tbody'),
+
   addTransaction(transaction, index) {
     const tr = document.createElement('tr');
     tr.innerHTML = DOM.innerHTMLTransaction(transaction)
 
     DOM.transactionsContainer.appendChild(tr)
   },
+
   innerHTMLTransaction(transaction){
     const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+    const amount = Utils.formatCurrency(transaction.amount)
+      
     const html = `
         <td class="description">${transaction.description}</td>
-        <td class="${CSSclass} ">${transaction.amount}</td>
+        <td class="${CSSclass} ">${amount}</td>
         <td class="date">${transaction.date}</td>
         <td>
           <img src="./assets/minus.svg" 
@@ -70,15 +108,60 @@ const DOM = {
         </td>
     `
     return html
-  }
+  },
+
+  updateBalance() {
+    document.getElementById('incomeDisplay').innerHTML = Utils.formatCurrency(Transaction.incomes())
+    document.getElementById('expenseDisplay').innerHTML = Utils.formatCurrency(Transaction.expenses())
+    document.getElementById('totalDisplay').innerHTML = Utils.formatCurrency(Transaction.total())
+  },
+
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = ""
+  },
 }
 
 const Utils = {
   formatCurrency(value) {
     const signal = Number(value) < 0 ? "-" : ""
+
+    value = String(value).replace(/\D/g, "")
+    value = Number(value) / 100
+    value = value.toLocaleString("pt-BR", {
+      style: "currency", 
+      currency: "BRL"
+    })
+
+    return signal + value
   }
 }
 
-transactions.forEach(function(transaction) {
-  DOM.addTransaction(transaction)
+const Form = {
+  submit(event) {
+    console.log(event)
+  }
+}
+
+const App = {
+  init() {
+    Transaction.all.forEach(transaction => {
+      DOM.addTransaction(transaction)
+    })
+    
+    DOM.updateBalance()
+  },
+
+  reload(){
+    DOM.clearTransactions()
+    App.init()
+  },
+}
+
+App.init()
+
+Transaction.add ({
+  description: 'alo',
+  amount: 200,
+  date: '21/01/2021'
 })
+
